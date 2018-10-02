1419fcf4619b1ecdf059a1892308e29240299375
xlfdll <xlfdll@gmail.com>
2018-10-01 22:10:55 -0700

    Fixed adding directory as file bug in drag and drop operations

--- BatchRenamer/Helpers/FileHelper.cs
+++ BatchRenamer/Helpers/FileHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace BatchRenamer
@@ -8,12 +9,28 @@ namespace BatchRenamer
     {
         public static void AddNewFiles(IEnumerable<String> fileNames)
         {
+            List<String> files = new List<String>();
+
             // Manually check original file name here to avoid broken item reference issue when removing files later on
             foreach (String fileName in fileNames)
             {
-                if (AppState.Current.Files.Where(f => (f.OriginalFullPath == fileName)).Count() == 0)
+                FileAttributes attributes = File.GetAttributes(fileName);
+
+                if (attributes.HasFlag(FileAttributes.Directory))
                 {
-                    AppState.Current.Files.Add(new BatchFileInfo(fileName));
+                    files.AddRange(Directory.GetFiles(fileName));
+                }
+                else
+                {
+                    files.Add(fileName);
+                }
+            }
+
+            foreach (String file in files)
+            {
+                if (AppState.Current.Files.Where(f => (f.OriginalFullPath == file)).Count() == 0)
+                {
+                    AppState.Current.Files.Add(new BatchFileInfo(file));
                 }
             }
         }
